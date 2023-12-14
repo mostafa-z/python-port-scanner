8b1cf52ec8497ee53250929939941b67f5679ba8
mostafa-z <mostafazarghami@gmail.com>
2023-12-14 22:13:09 +0330

    allows the user to choose between scanning a single IP or an IP range
    Depending on the choice made, it prompts for the required details (IP addresses and port numbers) and performs the port scanning accordingly. Adjust the number of threads (num_threads) based on system capabilities and network conditions for optimal performance.

--- port_scanner.py
+++ port_scanner.py
@@ -3,8 +3,9 @@ import threading
 import queue
 import itertools
 import sys
+import ipaddress
 
-def port_scan(target_ip, start_port, end_port, results):
+def port_scan(target_ip, start_port, end_port, port_queue, results):
     animation = itertools.cycle(['|', '/', '-', '\\'])
     while True:
         port = port_queue.get()
@@ -24,38 +25,67 @@ def port_scan(target_ip, start_port, end_port, results):
 
     sys.stdout.write('\n')
 
-# Get the target IP and port range from the user
-target_ip = input("Enter the IP address to scan: ")
-start_port = int(input("Enter the starting port number: "))
-end_port = int(input("Enter the ending port number: "))
+# Function to scan a single IP
+def single_ip_scan():
+    target_ip = input("Enter the IP address to scan: ")
+    start_port = int(input("Enter the starting port number: "))
+    end_port = int(input("Enter the ending port number: "))
+    scan(target_ip, start_port, end_port)
 
-num_threads = 100  # Number of threads to use for scanning
-port_queue = queue.Queue()
-results = {}
+# Function to scan a range of IPs
+def ip_range_scan():
+    start_ip = input("Enter the starting IP address: ")
+    end_ip = input("Enter the ending IP address: ")
+    start_port = int(input("Enter the starting port number: "))
+    end_port = int(input("Enter the ending port number: "))
+    
+    ip_range = ipaddress.summarize_address_range(ipaddress.IPv4Address(start_ip), ipaddress.IPv4Address(end_ip))
+    for network in ip_range:
+        for host in network:
+            scan(str(host), start_port, end_port)
 
-# Create worker threads
-threads = []
-for _ in range(num_threads):
-    thread = threading.Thread(target=port_scan, args=(target_ip, start_port, end_port, results))
-    thread.start()
-    threads.append(thread)
+# Common scan function used for both single IP and IP range scan
+def scan(target_ip, start_port, end_port):
+    num_threads = 100  # Number of threads to use for scanning
+    port_queue = queue.Queue()
+    results = {}
 
-# Fill the port queue
-for port in range(start_port, end_port + 1):
-    port_queue.put(port)
+    # Create worker threads
+    threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=port_scan, args=(target_ip, start_port, end_port, port_queue, results))
+        thread.start()
+        threads.append(thread)
 
-# Wait for all threads to complete
-port_queue.join()
+    # Fill the port queue
+    for port in range(start_port, end_port + 1):
+        port_queue.put(port)
 
-# Stop workers
-for _ in range(num_threads):
-    port_queue.put(None)
+    # Wait for all threads to complete
+    port_queue.join()
 
-for thread in threads:
-    thread.join()
+    # Stop workers
+    for _ in range(num_threads):
+        port_queue.put(None)
 
-# Sort and print open ports
-sorted_ports = sorted(results.items(), key=lambda x: x[0])
-print(f"Open ports on {target_ip}:")
-for port, status in sorted_ports:
-    print(f"Port {port}: {status}")
+    for thread in threads:
+        thread.join()
+
+    # Sort and print open ports
+    sorted_ports = sorted(results.items(), key=lambda x: x[0])
+    print(f"Open ports on {target_ip}:")
+    for port, status in sorted_ports:
+        print(f"Port {port}: {status}")
+
+# Main menu for selecting scan type
+print("Select the scan type:")
+print("1. Single IP")
+print("2. IP Range")
+scan_type = input("Enter your choice (1 or 2): ")
+
+if scan_type == "1":
+    single_ip_scan()
+elif scan_type == "2":
+    ip_range_scan()
+else:
+    print("Invalid choice. Please enter 1 or 2.")
